b0c221daf3e2e32910dc4c9ee9c74c6bcea64d00
Yeamin Foysal <yeaminfoysal14@gmail.com>
2025-10-04 19:28:51 +0600

    connect postgres database and run server on local complete

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,25 @@
 import http, { Server } from "http";
 import app from "./app";
 import dotenv from "dotenv";
+import { prisma } from "./config/db";
 
 dotenv.config();
 
 let server: Server | null = null;
 
+async function connectToDB() {
+  try {
+    await prisma.$connect();
+    console.log("✅Database connection successfull✅")
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
 async function startServer() {
   try {
+    await connectToDB();
     server = http.createServer(app);
     server.listen(process.env.PORT, () => {
       console.log(`🚀 Server is running on port ${process.env.PORT}`);
